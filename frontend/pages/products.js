3f5564cfc89b741b2596f6fafc97a1916d333c3c
Jhonattas Ferreira <jfs120@gmail.com>
2023-08-07 21:29:09 -0300

    feat: create the pagination component

--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,5 +1,12 @@
 import Products from '../components/Products';
+import Pagination from '../components/Pagination';
 
-const productsPage = () => <Products />;
+const productsPage = () => (
+  <div>
+    <Pagination page={1} />
+    <Products />
+    <Pagination page={1} />
+  </div>
+);
 
 export default productsPage;
