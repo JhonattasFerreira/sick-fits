e3e191c9169744c943f7318fbd42e86134a91139
Jhonattas Ferreira <jfs120@gmail.com>
2023-09-10 02:06:21 -0300

    feat: create comp of reset password

--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import SingIn from '../components/SignIn';
 import SingUp from '../components/SignUp';
+import RequestReset from '../components/RequestReset';
 
 const GridStyles = styled.div`
   display: grid;
@@ -12,6 +13,7 @@ const signInPage = () => (
   <GridStyles>
     <SingIn />
     <SingUp />
+    <RequestReset />
   </GridStyles>
 );
 
