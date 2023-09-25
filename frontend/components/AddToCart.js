6e9375346b55c2d2abffc705c6abfdaa9c123516
Jhonattas Ferreira <jfs120@gmail.com>
2023-09-25 20:25:20 -0300

    feat: create add to cart component

---
+++ frontend/components/AddToCart.js
@@ -0,0 +1,31 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import PropTypes from 'prop-types';
+import { CURRENT_USER_QUERY } from './User';
+
+const ADD_TO_CART_MUTATION = gql`
+  mutation ADD_TO_CART_MUTATION($id: ID!) {
+    addToCart(productId: $id) {
+      id
+    }
+  }
+`;
+
+const AddToCart = ({ id }) => {
+  const [addToCart, { loading }] = useMutation(ADD_TO_CART_MUTATION, {
+    variables: { id },
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  });
+
+  return (
+    <button disabled={loading} type="button" onClick={addToCart}>
+      Add{loading && 'ing'} to Cart
+    </button>
+  );
+};
+
+AddToCart.propTypes = {
+  id: PropTypes.string,
+};
+
+export default AddToCart;
--- frontend/components/Product.js
+++ frontend/components/Product.js
@@ -5,6 +5,7 @@ import Title from './styles/Title';
 import PriceTag from './styles/PriceTag';
 import FormatMoney from '../lib/formatMoney';
 import DeleteProduct from './DeleteProduct';
+import AddToCart from './AddToCart';
 
 const Product = ({ product }) => (
   <ItemStyles>
@@ -25,6 +26,7 @@ const Product = ({ product }) => (
       >
         Edit ✏️
       </Link>
+      <AddToCart id={product.id} />
       <DeleteProduct id={product.id}>Delete</DeleteProduct>
     </div>
   </ItemStyles>
